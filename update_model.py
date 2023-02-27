ef7743d298de037885dd147993a388978f69d36c
Jason Smith <jason2.smith@intel.com>
2023-02-26 21:31:45 -0500

    Update with AHF-specific modifications
    This patch updates the code to run for AHF KRACH generation.  It
    contains:
    - Support reading scores from JSON files.
    - Max iterations: 200
    - Error diff: 0.00001
    - Scale results with x10000 (rather than logarithmic scaling)
    - Couple minor numpy/panda tweaks due to newer version of modules.
    - Remove Google Sheet support.
    - Print results to console

--- update_model.py
+++ update_model.py
@@ -1,43 +1,76 @@
+#!/usr/bin/env python3
 import argparse
-import gspread
 import logging
 import numpy as np
 import os
 import pandas as pd
 import time
+import json
 
 from datetime import datetime
 from collections import Counter
-from oauth2client.service_account import ServiceAccountCredentials
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 DUMMY_PLAYER = 'DUMMY PLAYER'
 
+#----------------------------------------------------------------------------
+# Read AHF score data into a ledger. This is specific to the JSON format
+# provided by the gamesheetstats.com API. To use this script with a different
+# data source, you would replace this reader with one specific to your context.
+class AhfScoreReader:
+    def __init__(self):
+        pass
 
-def connect_sheet(creds_file, sheet_key):
-    scope = ['https://spreadsheets.google.com/feeds']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(creds_file, scope)
-    gc = gspread.authorize(credentials)
+    #----------------------------------------------------------------------------
+    def read(self, filename):
+        ledger = {
+            'Date'     : [],
+            'Player A' : [],
+            'Player B' : [],
+            'Wins A'   : [],
+            'Wins B'   : [],
+        }
+        with open(filename) as f:
+            jdata = json.load(f)
+            for game in jdata:
+                self.readGame(ledger, game['game'])
+        return ledger
 
-    return gc.open_by_key(sheet_key)
+    #----------------------------------------------------------------------------
+    def readGame(self, ledger, game):
+        date       = datetime.strptime(game['date'], "%b %d, %Y").date()
+        team1      = game['homeTeam']['name']
+        team1score = game['finalScore']['homeGoals']
+        team2      = game['visitorTeam']['name']
+        team2score = game['finalScore']['visitorGoals']
+        shootout   = any(map(lambda x: x['title'] == 'SO', game['scoresByPeriod']))
+
+        if team1score == team2score:
+            raise RuntimeError("Does not support ties")
+
+        winner = team1 if team1score > team2score else team2
+        loser  = team2 if team1score > team2score else team1
+
+        ledger['Date'].append(str(date.isoformat()))
+        ledger['Player A'].append(loser)
+        ledger['Player B'].append(winner)
+        ledger['Wins A'].append(0)
+        ledger['Wins B'].append(1)
 
 
-def extract_game_data(sheet, sheet_name):
-    worksheet = sheet.worksheet(sheet_name)
-    rawdata = worksheet.get_all_values()
+def extract_game_data(inputFile):
+    df = pd.DataFrame(AhfScoreReader().read(inputFile))
 
-    df = pd.DataFrame(rawdata[1:], columns=rawdata[0])
     assert all(c in df.columns for c in ['Date', 'Player A', 'Player B', 'Wins A', 'Wins B']), \
         'Expecting columns Date, Player A, Player B, Wins A, Wins B'
 
-    df['Date'] = df['Date'].astype(datetime)
+    #df['Date'] = df['Date'].astype(datetime)
     df['Wins A'] = df['Wins A'].astype(int)
     df['Wins B'] = df['Wins B'].astype(int)
 
     return df
-
 
 def add_dummy_games(game_data, alpha=1):
     ''' Regularizes the estimate by adding games against a dummy player.
@@ -54,18 +87,19 @@ def add_dummy_games(game_data, alpha=1):
 
     return df
 
-
-def compute_rank_scores(game_data, max_iters=1000, error_tol=1e-3):
+def compute_rank_scores(game_data, max_iters=200, error_tol=0.00001):
     ''' Computes Bradley-Terry using iterative algorithm
 
         See: https://en.wikipedia.org/wiki/Bradley%E2%80%93Terry_model
     '''
     # Do some aggregations for convenience
     # Total wins per player
-    winsA = game_data.groupby('Player A').agg(sum)['Wins A'].reset_index()
+    #winsA = game_data.groupby('Player A').agg(sum)['Wins A'].reset_index()
+    winsA = game_data.groupby('Player A')[['Wins A']].agg(sum).reset_index()
     winsA = winsA[winsA['Wins A'] > 0]
     winsA.columns = ['Player', 'Wins']
-    winsB = game_data.groupby('Player B').agg(sum)['Wins B'].reset_index()
+    #winsB = game_data.groupby('Player B').agg(sum)['Wins B'].reset_index()
+    winsB = game_data.groupby('Player B')[['Wins B']].agg(sum).reset_index()
     winsB = winsB[winsB['Wins B'] > 0]
     winsB.columns = ['Player', 'Wins']
     wins = pd.concat([winsA, winsB]).groupby('Player').agg(sum)['Wins']
@@ -83,9 +117,9 @@ def compute_rank_scores(game_data, max_iters=1000, error_tol=1e-3):
     for iters in range(max_iters):
         oldranks = ranks.copy()
         for player in ranks.index:
-            denom = np.sum(num_games[tuple(sorted([player, p]))]
+            denom = np.sum(np.fromiter((num_games[tuple(sorted([player, p]))]
                            / (ranks[p] + ranks[player])
-                           for p in ranks.index if p != player)
+                           for p in ranks.index if p != player), dtype=float))
             ranks[player] = 1.0 * wins[player] / denom
 
         ranks /= sum(ranks)
@@ -94,80 +128,42 @@ def compute_rank_scores(game_data, max_iters=1000, error_tol=1e-3):
             break
 
     if np.sum((ranks - oldranks).abs()) < error_tol:
-        logging.info(" * Converged after %d iterations.", iters)
+        logging.debug(" * Converged after %d iterations.", iters)
     else:
-        logging.info(" * Max iterations reached (%d iters).", max_iters)
+        logging.debug(" * Max iterations reached (%d iters).", max_iters)
 
     del ranks[DUMMY_PLAYER]
 
     # Scale logarithm of score to be between 1 and 1000
     ranks = ranks.sort_values(ascending=False) \
-                 .apply(lambda x: np.log1p(1000 * x) / np.log1p(1000) * 1000) \
+                 .apply(lambda x: x * 10000.0 + 0.5) \
                  .astype(int) \
                  .clip(1)
 
     return ranks
 
 
-def upload_to_gsheets(sheet, ranks, rank_sheet):
-    ranking_ws = [w for w in sheet.worksheets() if w.title == rank_sheet]
-    if not ranking_ws:
-        worksheet = sheet.add_worksheet(title=rank_sheet, rows=100, cols=25)
-    else:
-        worksheet = ranking_ws[0]
-
-    worksheet.clear()
-    worksheet.update_cell(1, 1, 'Player')
-    worksheet.update_cell(1, 2, 'Score')
-
-    if worksheet.row_count < len(ranks) + 1:
-        worksheet.resize(rows=len(ranks) + 1, cols=25)
-
-    player_cells = worksheet.range(2, 1, len(ranks) + 1, 1)
-    for cell, player in zip(player_cells, ranks.index):
-        cell.value = player
-
-    value_cells = worksheet.range(2, 2, len(ranks) + 1, 2)
-    for cell, val in zip(value_cells, ranks.values):
-        cell.value = val
-
-    worksheet.update_cells(player_cells + value_cells)
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Script to update Google Sheet with ranking model")
-    parser.add_argument('--creds-file', type=str, required=True,
-                        help='Google Drive API sheet')
-    parser.add_argument('--sheet-key', default=None, type=str, help='Google sheet key from URL')
-    parser.add_argument('--backup-dir', default=None, type=str,
-                        help='dir to backup game data')
-    parser.add_argument('--data-sheet', default='Game Data', type=str,
-                        help='Name of worksheet containing game data')
-    parser.add_argument('--rank-sheet', default='Ranking', type=str,
-                        help='Name of worksheet to write rankings')
-    parser.add_argument('--alpha', default=1, type=int, help='Regularization parameter')
+    parser.add_argument('--alpha', default=0.85, type=float, help='Regularization parameter')
+    parser.add_argument('--debug', action='store_true', help="Enable debug logging")
+    parser.add_argument('input', help="JSON file containing score data to analyze")
     args = parser.parse_args()
 
-    logging.info("Connecting to Google sheets '%s'...", args.sheet_key)
-    sheet = connect_sheet(args.creds_file, args.sheet_key)
+    if args.debug:
+        logging.getLogger().setLevel(logging.DEBUG)
 
-    logging.info("Extracting game data from '%s'...", args.data_sheet)
-    game_data = extract_game_data(sheet, args.data_sheet)
-    logging.info(" * Found %d rows.", len(game_data))
+    logging.debug("Extracting game data from '%s'...", args.input)
+    game_data = extract_game_data(args.input)
+    logging.debug(" * Found %d rows.", len(game_data))
 
-    if args.backup_dir:
-        filename = "game_data-%d.csv.gz" % int(time.time())
-        logging.info(" * Saving backup to '%s'", filename)
-        game_data.to_csv(os.path.join(args.backup_dir, filename),
-                         compression='gzip', index=False)
-
-    logging.info("Adding dummy game for regularization (alpha=%d)...", args.alpha)
+    logging.debug("Adding dummy game for regularization (alpha={})...".format(args.alpha))
     game_data = add_dummy_games(game_data, args.alpha)
 
-    logging.info("Computing rank scores...")
+    logging.debug("Computing rank scores...")
     ranks = compute_rank_scores(game_data)
 
-    logging.info("Uploading rank scores to sheet '%s'...", args.rank_sheet)
-    upload_to_gsheets(sheet, ranks, args.rank_sheet)
+    # TODO: print
+    for rank in ranks.items():
+        print("{:<30} : {:>5}".format(rank[0], rank[1]))
 
-    logging.info("Done")
